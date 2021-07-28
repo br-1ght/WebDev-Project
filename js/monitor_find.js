5a9692a61fdbc91ed479170fa57cd85a4a386a2d
br-1ght <fcw2k16@gmail.com>
2021-07-28 15:49:27 +0800

    28/7/21

--- js/monitor_find.js
+++ js/monitor_find.js
@@ -1,13 +1,23 @@
 function monitor_find() {
-    var search_form = document.forms[0]; 
-    let budget_choice = search_form.budget.value;
-    let useCase_choice = search_form.use_case.value;
-    let resolution_choice = search_form.resolution.value;
-    let screenSize_choice = search_form.screen_size.value;
-    let adjustableStand_choice = search_form.adjustable_stand.value;
-    var recommendation_div = document.createElement("DIV");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "json/monitors.json");
+    xhr.onload = function() {
+        if (this.status == 200) {
+            var monitorData = JSON.parse(xhr.responseText);
+            console.log(monitorData);
+        }
+    }
+    xhr.send();
+
+    let search_form = document.forms[0]; 
+    let budget = search_form.budget.value;
+    let useCase = search_form.use_case.value;
+    let resolution = search_form.resolution.value;
+    let screenSize = search_form.screen_size.value;
+    let adjustableStand = search_form.adjustable_stand.value;
+    let recommendation_div = document.createElement("DIV");
     recommendation_div.className = "recommendation_item";
     // recommendation_div.innerHTML = "budget: " + budget_choice + ", Use Case: " + useCase_choice;
-    document.getElementById("recommendations").appendChild(recommendation_div)
-    search_form.reset()
+    document.getElementById("recommendations").appendChild(recommendation_div);
+    search_form.reset();
 }
