11c40b25b31b34ab5fd2e825625959e71afffc62
br-1ght <fcw2k16@gmail.com>
2021-08-04 14:58:44 +0800

    4/8/21

--- js/feedback.js
+++ js/feedback.js
@@ -19,12 +19,12 @@ document.getElementById("feedback_form").addEventListener("submit", submitForm);
 // Send to firebase
 function submitForm(event) {
   event.preventDefault();
-  let name = getUserInput("Name")
-  let email = getUserInput("Email")
-  let reason = getUserInput("reason")
-  let suggestion = getUserInput("suggestion")
-  dataToFirebase(name, email, reason, suggestion)
-  window.location.replace("feedback_success.html")
+  let name = getUserInput("Name");
+  let email = getUserInput("Email");
+  let reason = getUserInput("reason");
+  let suggestion = getUserInput("suggestion");
+  dataToFirebase(name, email, reason, suggestion);
+  window.location.replace("feedback_success.html");
 }
 
 // Get value of user input
--- js/monitor_find.js
+++ js/monitor_find.js
@@ -8,7 +8,7 @@ function monitor_search() {
             let monitorData = JSON.parse(xhr.responseText);
 
             let recommendations = document.getElementById("recommendations");
-            recommendations.innerHTML = "" // Clear all the previous recommendations if user wants to search again
+            recommendations.innerHTML = ""; // Clear all the previous recommendations if user wants to search again
 
             // Get data from user's input 
             let search_form = document.forms[0]; 
@@ -16,10 +16,10 @@ function monitor_search() {
 
             let useCase = []; // useCase is an array so that if the user picks both gaming and work options, it will store 2 values
             if (document.getElementById("workCheck").checked) {
-                useCase.push(document.getElementById("workCheck").value)
+                useCase.push(document.getElementById("workCheck").value);
             }
             if (document.getElementById("gamingCheck").checked) {
-                useCase.push(document.getElementById("gamingCheck").value)
+                useCase.push(document.getElementById("gamingCheck").value);
             }
 
             let resolution = search_form.resolution.value;
@@ -32,19 +32,19 @@ function monitor_search() {
                 return false; // Prevent the form from being submitted
             }
             if (useCase.length === 0) { // Check if use case array is empty
-                alert("Please select at least 1 choice for use case!")
+                alert("Please select at least 1 choice for use case!");
                 return false;
             }
             if (resolution == "") {
-                alert("Resolution must be selected!")
+                alert("Resolution must be selected!");
                 return false;
             }
             if (screenSize == "") {
-                alert("Screen size must be selected!")
+                alert("Screen size must be selected!");
                 return false;
             }
             if (adjustableStand == "") {
-                alert("Please choose an option whether you need an adjustable stand!")
+                alert("Please choose an option whether you need an adjustable stand!");
                 return false;
             }
 
@@ -58,74 +58,71 @@ function monitor_search() {
             }
             
             let userChoiceObj = new userChoice(budget, useCase, resolution, screenSize, adjustableStand);
-            console.log(userChoiceObj);
             
             // Compare user's inputs to the monitors specifications and then match count will increase if user input matches the same as the monitors
             var matches = [] // To store match results, also indexes of the array is correlated to indexes of the monitor data
             for (i=0; i < monitorData.length; i++) { 
-               matchCount = 0
+               matchCount = 0;
                if (userChoiceObj.budget >= monitorData[i].budget) {
-                   matchCount += 1
+                   matchCount += 1;
                }
                /* Match count numbers for each use case have these values so that match results are distinguishable
                e.g any monitor that has a match count of 100+ is a work monitor, 200+ is gaming monitor and 300+ is both a gaming and work monitor */
                for (j = 0; j < userChoiceObj.useCase.length; j++) { 
                    if (userChoiceObj.useCase[j] === monitorData[i].useCase[j] && monitorData[i].useCase[j] === "Work") {
-                       matchCount += 100
+                       matchCount += 100;
                    }
 
                    if (userChoiceObj.useCase[j] === monitorData[i].useCase[0] && monitorData[i].useCase[0] === "Gaming") {
-                       matchCount += 200
+                       matchCount += 200;
                    }
 
                    if (userChoiceObj.useCase[j] === monitorData[i].useCase[1] && monitorData[i].useCase[1] === "Gaming") {
-                       matchCount += 200
+                       matchCount += 200;
                    }
                }
                if (userChoiceObj.resolution === monitorData[i].resolution) {
-                   matchCount += 1
+                   matchCount += 1;
                }
                if (userChoiceObj.screenSize === monitorData[i].screenSize) {
-                   matchCount += 1
+                   matchCount += 1;
                }
                if (userChoiceObj.adjustableStand === monitorData[i].adjustableStand) {
-                   matchCount += 1
+                   matchCount += 1;
                }
                matches.push(matchCount)
             }
-            console.log(matches)
             // Get suitable results
-            let highestMatch = Math.max.apply(null, matches) // Find the number of the highest match count
-            let indexesOfHighest = [] // Since there could be multiple monitors that can have the highest match count, store the index in an array
+            let highestMatch = Math.max.apply(null, matches); // Find the number of the highest match count
+            let indexesOfHighest = []; // Since there could be multiple monitors that can have the highest match count, store the index in an array
             for (i=0; i< matches.length; i++) {
                 if (matches[i] === highestMatch) {
-                    indexesOfHighest.push(i)
+                    indexesOfHighest.push(i);
                 }
             }
-            console.log(indexesOfHighest)
             
             // Showing the recommendations
             for (i = 0; i < indexesOfHighest.length; i++) { 
 
                 // Monitor specification details
-                let recoModel = monitorData[indexesOfHighest[i]].model // monitor name for anchor link and image
-                let recoModelName = recoModel.replaceAll("-", " ") // Display the monitor name without the dashes(-)
-                let recoModelUseCase = monitorData[indexesOfHighest[i]].useCase
-                let recoModelRes = monitorData[indexesOfHighest[i]].resolution
-                let recoModelRR = monitorData[indexesOfHighest[i]].refreshRate
-                let recoModelSize = monitorData[indexesOfHighest[i]].screenSize
-                let recoModelPrice = monitorData[indexesOfHighest[i]].price
-                let recoModelAS = monitorData[indexesOfHighest[i]].adjustableStand
+                let recoModel = monitorData[indexesOfHighest[i]].model; // monitor name for anchor link and image
+                let recoModelName = recoModel.replaceAll("-", " "); // Display the monitor name without the dashes(-)
+                let recoModelUseCase = monitorData[indexesOfHighest[i]].useCase;
+                let recoModelRes = monitorData[indexesOfHighest[i]].resolution;
+                let recoModelRR = monitorData[indexesOfHighest[i]].refreshRate;
+                let recoModelSize = monitorData[indexesOfHighest[i]].screenSize;
+                let recoModelPrice = monitorData[indexesOfHighest[i]].price;
+                let recoModelAS = monitorData[indexesOfHighest[i]].adjustableStand;
 
                 // Create the results of recommended monitors
-                let recoDiv = document.createElement("DIV")
-                recoStyle = recoDiv.classList.add("recommendation_item")
+                let recoDiv = document.createElement("DIV");
+                recoStyle = recoDiv.classList.add("recommendation_item");
                 // Show image of the monitor, link to the page of the monitor, brief specification list of the monitor
-                recoDiv.innerHTML = "<img src='images/" + recoModel + ".jpg' height=200px>" + "<a href='monitors/" + recoModel + ".html' target='_blank'><h1>" + recoModelName + "</h1></a>" + "<div class='recommendation_spec_list'><p>Specifications</p><ul><li>Price: " + recoModelPrice + "</li><li>Use Case: " + recoModelUseCase + "</li><li>Resolution: " + recoModelRes + "</li><li>Screen Size: " + recoModelSize + "</li><li>Refresh rate: " + recoModelRR + "</li><li>Adjustable Stand: " + recoModelAS + "</li></ul></div>" 
-                recommendations.appendChild(recoDiv)
+                recoDiv.innerHTML = "<img src='images/" + recoModel + ".jpg' height=200px>" + "<a href='monitors/" + recoModel + ".html' target='_blank'><h1>" + recoModelName + "</h1></a>" + "<div class='recommendation_spec_list'><p>Specifications</p><ul><li>Price: " + recoModelPrice + "</li><li>Use Case: " + recoModelUseCase + "</li><li>Resolution: " + recoModelRes + "</li><li>Screen Size: " + recoModelSize + "</li><li>Refresh rate: " + recoModelRR + "</li><li>Adjustable Stand: " + recoModelAS + "</li></ul></div>";
+                recommendations.appendChild(recoDiv);
             }
-            search_form.reset() // Form resets after completing
+            search_form.reset(); // Form resets after completing
         }
     }
     xhr.send();
-};
+}
